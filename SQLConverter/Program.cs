96f25c0cde41a369d3bf113e49b4509057205d86
nkmnhan <nkmnhan@gmail.com>
2023-05-29 12:22:29 +0700

    Add cleanup.sql

--- SQLConverter/Program.cs
+++ SQLConverter/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using SQLConverter;
 
 
@@ -10,7 +10,7 @@ if (string.IsNullOrEmpty(workSpace))
 }
 
 var allFiles = Directory.GetFiles(workSpace).Where(x => x.EndsWith(".data"));
-
+var cleanUpContents = new List<string>();
 foreach (var file in allFiles)
 {
     var data = await File.ReadAllLinesAsync(file);
@@ -18,6 +18,19 @@ foreach (var file in allFiles)
     var newPath = file.Replace(".data", ".sql");
     await File.WriteAllLinesAsync(newPath, content);
     Console.WriteLine($"CreateNewFile: {newPath}");
+
+    var cleanupContent = TxTToSQL.Cleanup(data);
+    if (!string.IsNullOrEmpty(cleanupContent))
+    {
+        cleanUpContents.Add(cleanupContent);
+    }
+}
+
+if (cleanUpContents.Count > 0)
+{
+    var cleanupPath = $"{workSpace}/cleanup.sql";
+    await File.WriteAllLinesAsync(cleanupPath, cleanUpContents);
+    Console.WriteLine($"CreateNewFile: {cleanupPath}");
 }
 Console.WriteLine("--Done!--");
 
--- SQLConverter/TxTToSQL.cs
+++ SQLConverter/TxTToSQL.cs
@@ -15,11 +15,32 @@
             {
                 var values = $"{data[i].Replace("\t", "','")}";
                 var valueStatements = $"('{values}'),".Replace("'NULL'", "NULL");
-
+                if (i == data.Length - 1)
+                {
+                    valueStatements = valueStatements.Remove(valueStatements.Length - 1);
+                }
                 sqlStatements.Add(valueStatements);
             }
 
             return sqlStatements;
+        }
+
+        public static string Cleanup(string[]? data)
+        {
+            var tableName = data[0];
+            var statement = $"DELETE FROM {tableName} WHERE Id IN";
+
+            var ids = new List<string>();
+            for (int i = 2; i < data.Length; i++)
+            {
+                var splits = data[i].Split("\t");
+                if (!string.IsNullOrWhiteSpace(splits[0]) && Guid.TryParse(splits[0], out var x))
+                {
+                    ids.Add(splits[0]);
+                }
+            }
+
+            return ids.Any() ? $"{statement} ('{string.Join("','", ids)}')" : "";
         }
     }
 }
